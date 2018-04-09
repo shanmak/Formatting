ecbf80fdd35b47bf7c81202078db53d8769e4c0c
Manikandan Shanmugam <e0284006@u.nus.edu>
2018-04-09 22:09:59 +0800

    formatting

--- Formatting/Program.cs
+++ Formatting/Program.cs
@@ -10,6 +10,17 @@ namespace Formatting
     {
         static void Main(string[] args)
         {
+            string one = "one";
+            string two = "two";
+            string three = "three";
+            string one1 = "one1";
+            string two1 = "two1";
+            string three1 = "three1";
+
+            Console.WriteLine(string.Format("{0}{1}{2}", one.PadRight(10, ' '), two.PadRight(10, ' '), three.PadRight(10, ' ')));
+            Console.WriteLine();
+            Console.WriteLine(string.Format("{0,-10}{1,10}{2,10}", one1.PadRight(10, ' '), two1.PadRight(10, ' '), three1.PadRight(10, ' ')));
+
         }
     }
 }
